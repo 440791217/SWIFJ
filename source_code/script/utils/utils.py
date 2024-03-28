b5c4681dcd7a6ecc31025274e32b5d103bc47680
UltramanWang <156871160+UltramanWang@users.noreply.github.com>
2024-03-28 19:47:38 +0800

    增加判断串口文件

--- source_code/script/utils/utils.py
+++ source_code/script/utils/utils.py
@@ -1,14 +1,24 @@
 import binascii
+import zlib
  
 def crc32(data):
     return binascii.crc32(data) & 0xffffffff
 
 def get_config_path():
-    return ''
-
+    return 'D:\\Project\\Project\\SWIFJ\\config'
 # def getCWD():
     # return 
 
+def calculate_crc32(file_path):
+    # 打开文件并计算 CRC32 校验值
+    with open(file_path, 'rb') as file:
+        crc_value = 0
+        while True:
+            data = file.read(1024)  # 读取文件数据块
+            if not data:
+                break
+            crc_value = zlib.crc32(data, crc_value)  # 计算 CRC32 校验值
+    return crc_value
  
 
 
@@ -20,3 +30,4 @@ if __name__=='__main__':
     print(data1)
     # checksum = crc32(data)
     # print(f"CRC-32: {checksum:#010x}")
+
