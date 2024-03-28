2ceed6d4b034fbc7ce465495c5df022d4aca4ec5
440791217 <440791217@qq.com>
2024-03-28 22:39:53 +0800

    config

--- source_code/script/utils/utils.py
+++ source_code/script/utils/utils.py
@@ -1,11 +1,35 @@
 import binascii
 import zlib
+import os
+import platform
  
+def get_home_path():
+    f=''
+    if platform.system() == "Windows":
+        f= os.path.join(os.environ['USERPROFILE'], '')
+    elif platform.system() == "Linux":
+        f= os.path.join(os.path.expanduser('~'), '')
+    elif platform.system() == "Darwin":
+        f= os.path.join(os.path.expanduser('~'), '')
+    else:
+        raise OSError('Unsupported operating system')
+    home=os.path.join(f,'.SWIFJ')
+    print(home)
+    if not os.path.exists(home):
+        os.mkdir(home)   
+    return home
+
+
+
+
 def crc32(data):
     return binascii.crc32(data) & 0xffffffff
 
 def get_config_path():
-    return 'D:\\Project\\Project\\SWIFJ\\config'
+    f=os.path.join(get_home_path(),'config')
+    if not os.path.exists(f):
+        os.mkdir(f)  
+    return f
 # def getCWD():
     # return 
 
@@ -28,6 +52,8 @@ if __name__=='__main__':
     print(data)
     data1 = "Hello, World!".encode('utf-8')
     print(data1)
+    desktop_path = get_home_path()
+    # print(desktop_path)
     # checksum = crc32(data)
     # print(f"CRC-32: {checksum:#010x}")
 
