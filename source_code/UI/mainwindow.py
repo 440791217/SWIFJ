a50858e903b53c98dcb166e02756db1b18ebd60f
UltramanWang <156871160+UltramanWang@users.noreply.github.com>
2024-04-02 16:56:05 +0800

    修改串口连接

--- source_code/UI/mainwindow.py
+++ source_code/UI/mainwindow.py
@@ -1,10 +1,15 @@
+import json
+import os
+import sys
 from PyQt5.QtWidgets import QMainWindow, QMessageBox,QPlainTextEdit
 from ui.ui_mainwindow import Ui_MainWindow
 from settingdialog import SettingDialog
 from fault_injection import SettingFault
 from connect import Connect
 from PyQt5.QtSerialPort import QSerialPort
-from PyQt5.QtCore import QIODevice,Qt
+from PyQt5.QtCore import QIODevice
+from source_code.script.utils import utils
+from loguru import logger
  
 class MainWindow(QMainWindow):
     def __init__(self,parent=None):
@@ -41,15 +46,81 @@ class MainWindow(QMainWindow):
         QMessageBox.about(self, '关于', '这是一个关于对话框示例')
 
     def open_serial_port(self):
-        s = self.m_settings.setting()
-        self.m_serial.setPortName(s.name)
-        self.m_serial.setBaudRate(s.baud_rate)
-        self.m_serial.setParity(s.parity)
-        self.m_serial.setDataBits(s.data_bits)
-        self.m_serial.setStopBits(s.stop_bits)
-        self.m_serial.setFlowControl(s.flow_control)
+        uart_file=utils.get_dir_path('config')
+        file_uart=os.path.join(uart_file,'uart.json')
+        if os.path.exists(file_uart):
+            logger.debug('文件存在')
+            with open(file_uart, 'rb') as f:
+                value = json.load(f)
+            crc32_value = value['crc']
+            data = value['data']
+            value_crc = utils.crc32_of_string(data)
+            logger.debug('value_crc',value_crc)
+            if crc32_value == value_crc:
+                logger.debug('crc校验值相等')
+                with open(file_uart, 'r') as file:
+                    data = json.load(file)
+                self.m_serial.setPortName(str(data['data']['uart']['com']))
+                self.m_serial.setBaudRate(int(data['data']['uart']['baud_rate']))
+                parity_text  = str(data['data']['uart']['Parity'])
+                if parity_text == "None":
+                     parity = QSerialPort.NoParity
+                elif parity_text == "Even":
+                    parity = QSerialPort.EvenParity
+                elif parity_text == "Odd":
+                    parity = QSerialPort.OddParity
+                elif parity_text == "Mark":
+                    parity = QSerialPort.MarkParity
+                elif parity_text == "Space":
+                    parity = QSerialPort.SpaceParity
+                else:
+                    parity = QSerialPort.NoParity
+                self.m_serial.setParity(parity)
+
+                databits_text = str(data['data']['uart']['dataBits'])
+                if databits_text == "5":
+                    databits = QSerialPort.Data5
+                elif databits_text == "6":
+                    databits = QSerialPort.Data6
+                elif databits_text == "7":
+                    databits = QSerialPort.Data7
+                elif databits_text == "8":
+                    databits = QSerialPort.Data8
+                else:
+                    databits = QSerialPort.Data8
+                self.m_serial.setDataBits(databits)
+
+                stopbits_text = str(data['data']['uart']['stopBits'])
+                if stopbits_text == "1":
+                    stopbits = QSerialPort.OneStop
+                elif stopbits_text == "1.5":
+                    stopbits = QSerialPort.OneAndHalfStop
+                elif stopbits_text == "2":
+                    stopbits = QSerialPort.TwoStop
+                else:
+                    stopbits = QSerialPort.OneStop
+                self.m_serial.setStopBits(stopbits)
+
+                flowcontrol_text = str(data['data']['uart']['flowControl'])
+                if flowcontrol_text == "NoFlowControl":
+                    flowcontrol = QSerialPort.NoFlowControl
+                elif flowcontrol_text == "HardwareControl":
+                    flowcontrol = QSerialPort.HardwareControl
+                elif flowcontrol_text == "SoftwareControl":
+                    flowcontrol = QSerialPort.SoftwareControl
+                else:
+                    flowcontrol = QSerialPort.NoFlowControl
+                self.m_serial.setFlowControl(flowcontrol)
+            else:
+                logger.debug('crc校验值不相等')
+                # self.fill_ports_parameters() 
+        else:
+            logger.debug('文件不存在')
+            # self.fill_ports_parameters()
+
         try:
-            if self.m_serial.open(QIODevice.ReadWrite):
+            # if self.m_serial.open(QIODevice.ReadWrite):
+            if self.m_serial.open(QSerialPort.ReadWrite):
                 self.m_ui.actionConnect.setEnabled(False)
                 self.m_ui.actionDisconnect.setEnabled(True)
                 self.m_ui.actionConfigure.setEnabled(False)
